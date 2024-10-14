782b69ea67d2a55b074a5a4fc6894dc09405ea47
brenosss <brenoe133@gmail.com>
2024-10-14 10:49:50 -0300

    Setup postgres

--- djangotemplate/settings.py
+++ djangotemplate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "djangotemplate.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_NAME", "django_template"),
+        "USER": os.getenv("POSTGRES_USER", "django_template"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "django_template"),
+        "HOST": os.getenv("POSTGRES_HOST", "postgres"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 

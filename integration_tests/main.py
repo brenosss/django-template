e5af38c030395f7bec2a8017ba862b4601cb06f0
brenosss <brenoe133@gmail.com>
2024-10-24 11:09:29 -0300

    Improve integration tests

--- integration_tests/main.py
+++ integration_tests/main.py
@@ -19,31 +19,48 @@ def test_create_cart_with_invalid_total_amount():
 def test_create_cart():
     response = _request("POST", "carts", {"total_amount": "5.50"})
     assert response.status_code == 201
-    assert response.json()["cart_id"] == 1
+    assert response.json()["cart_id"] is not None
 
 
 def test_get_all_carts():
+    new_cart_id = _request("POST", "carts", {"total_amount": "5.50"}).json()["cart_id"]
     response = _request("GET", "carts")
     carts = response.json()["carts"]
     assert response.status_code == 200
-    assert carts[0]["id"] == 1
-    assert carts[0]["total_amount"] == "5.50"
+    assert new_cart_id in [cart["id"] for cart in carts]
+
+
+def test_checkout_cart_with_invalid_cart_id():
+    response = _request("PUT", "carts/invalid/checkout")
+    assert response.status_code == 404
 
 
 def test_checkout_cart():
-    response = _request("PUT", "carts/1/checkout")
+    cart_id_under_test = _request("POST", "carts", {"total_amount": "5.50"}).json()[
+        "cart_id"
+    ]
+
+    response = _request("PUT", f"carts/{cart_id_under_test}/checkout")
     assert response.status_code == 200
+
+    response = _request("GET", "carts")
+    assert response.status_code == 200
+    carts = response.json()["carts"]
+    cart_under_test = next(cart for cart in carts if cart["id"] == cart_id_under_test)
+    assert cart_under_test["closed"] == True
 
     time.sleep(5)
 
-    response = _request("GET", "carts")
-    assert response.json()["carts"][0]["closed"] == True
-
     response = _request("GET", "orders")
     assert response.status_code == 200
-    assert response.json()["orders"][0]["cart_id"] == 1
+    orders = response.json()["orders"]
+    assert cart_id_under_test in [order["cart_id"] for order in orders]
 
     response = _request("GET", "metrics")
     assert response.status_code == 200
-    assert response.json()["metrics"][0]["description"] == "orders_created"
-    assert response.json()["metrics"][0]["count"] == 1
+    metrics = response.json()["metrics"]
+    assert "orders_created" in [metric["description"] for metric in metrics]
+    orders_created_metric = next(
+        metric for metric in metrics if metric["description"] == "orders_created"
+    )
+    assert orders_created_metric["count"] == 1
